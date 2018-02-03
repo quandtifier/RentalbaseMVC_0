848c9ad961d98cac383b0ebb066ed189fd70574a
Michael Quandt <mdquandt@hotmail.com>
2018-02-03 09:27:18 -0800

    Add project files.

---
+++ Rentalbase/DAL/RBaseContext.cs
@@ -0,0 +1,36 @@
+﻿using Rentalbase.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Rentalbase.DAL
+{
+    public class RBaseContext : DbContext
+    {
+        public RBaseContext() : base("RBaseContext")
+        {
+        }
+
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<Landord> Landlords { get; set; }
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<Lease> Leases { get; set; }
+        public DbSet<Invoice> Invoice { get; set; }
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+        public DbSet<InvoiceType> InvoiceTypes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Lease>()
+                .HasMany(l => l.Tenants).WithMany(t => t.Leases)
+                .Map(v => v.MapLeftKey("TenantID")
+                .MapRightKey("LeaseID")
+                .ToTable("TenantLease"));
+        }
+    }
+}
---
+++ Rentalbase/Models/Property.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Rentalbase.Models
+{
+    public class Property
+    {
+        public int ID { get; set; }
+        public int LandlordID { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public int Value{ get; set; }
+        public string Description { get; set; }
+        [ForeignKey("PropertyType")]
+        public string Type { get; set; }
+
+        public virtual Landord MyLandlord { get; set; }
+        public virtual ICollection<Invoice> Invoices { get; set; }
+        public virtual ICollection<Lease> Leases { get; set; }
+        public virtual ICollection<Tenant> Tenants { get; set; }
+    }
+}
